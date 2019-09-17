d5e5e4446f705976d56823daf9aad34a7198e872
Justin Stewart <Justin.lighthouse.labs@gmail.com>
2019-09-17 20:51:57 +0000

    made Assert Arrays and fixed other bugs

--- eqArrays.js
+++ eqArrays.js
@@ -2,7 +2,7 @@ const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`❌❌❌Assertion Passed: ${actual} !== ${expected}`);
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
@@ -18,8 +18,8 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "3"]), false) // => false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "3"]), false); // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
