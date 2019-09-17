ab20706fc6a1c6ec11cf552b13946dd6ad368b37
Justin Stewart <Justin.lighthouse.labs@gmail.com>
2019-09-17 21:59:34 +0000

    made flatten

---
+++ flatten.js
@@ -0,0 +1,39 @@
+const flatten = function(arr) {
+  let answer = [];
+  if (arr.length === 0) {
+    return arr;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      for (let j = 0; j < arr[i].length; j++) {
+        answer.push(arr[i][j]);
+      }
+    } else {
+      answer.push(arr[i]);
+    }
+  }
+  return answer;
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length != arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+assertArraysEqual(flatten([1, 2, [3, 4], 5]), [1, 2, 3, 4, 5]);
