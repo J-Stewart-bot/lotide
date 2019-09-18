6db05439018e733c4f8b7e8a51f0a261b357bc39
Justin Stewart <Justin.lighthouse.labs@gmail.com>
2019-09-18 20:11:46 +0000

    created countLetters

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  const results = {};
+
+  for (const letter of sentence) {
+    if (results[letter]) {
+      results[letter] += 1;
+    } else {
+      results[letter] = 1;
+    }
+  }
+
+  return results;
+
+}
+
+countLetters("Lighthouse in the house");
