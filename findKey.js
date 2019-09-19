b7b784ec0fcbbac93b8f3f0a5153fd6f27846d11
Justin Stewart <Justin.lighthouse.labs@gmail.com>
2019-09-19 19:28:58 +0000

    created findKey

---
+++ findKey.js
@@ -0,0 +1,17 @@
+const findKey = function(list, func) {
+  for (const index of Object.keys(list)) {
+    if (func(list[index])) {
+      return index;
+    }
+  }
+}
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)); // => "noma"
+
