a19b68e531a29a761d07e7cd4e5c5a1a43e3aad1
Justin Stewart <Justin.lighthouse.labs@gmail.com>
2019-09-23 21:38:43 +0000

    eqArrays completed + recursion added

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,8 +1,14 @@
-const assertEqual = require('../assertEqual');
-const eqArrays = require('../eqArrays');
+const assert = require('chai').assert;
+const eqArrays   = require('../eqArrays');
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "3"]), false); // => false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+describe("#eqArrays", () => {
+  it("returns true", () => {
+    assert.deepEqual(eqArrays([1, [2], 3], [1, [2], 3]), true);
+  });
+  it("returns true", () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+  it("returns true", () => {
+    assert.deepEqual(eqArrays([1, [2, [3]]], [1, [2, [3]]]), true);
+  });
+});
